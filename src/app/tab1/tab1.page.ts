3865e2ea0737de46bc6d11efcdf612d0e7dc5917
Aranguiz Rios <Davis.Aranguiz@la.imtn.com>
2022-12-01 12:56:20 -0300

    se agrega proyecto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,53 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
+
+  async AgregarLista() {
+    let alerta = await this.alertController.create({
+      header:"Agregar lista",
+      inputs:[
+        {
+          type:"text",
+          name:"titulo",
+          placeholder:"Ingresar nombre de la lista"
+        }
+      ],
+      buttons: [
+        {
+          text:"cancelar",
+          role:"cancel"
+        },
+        {
+          text:"crear",
+          handler: (data: any)=>{
+            this.validInput(data);
+              
+          }
+        }
+      ]
+    });
+    await alerta.present();
+    console.log("click en boton");
+  }
+  validInput(input: any): boolean{
+    if (input && input.titulo){
+      return true
+    }
+    this.presentToast("Debe ingresar un valor");
+    return false;
+  }
+
+ async presentToast(mensage: string){
+   let toast = await this.toastController.create({
+    message: mensage,
+    duration: 2000
+   });
+   toast.present();
+
+ }
 
 }
